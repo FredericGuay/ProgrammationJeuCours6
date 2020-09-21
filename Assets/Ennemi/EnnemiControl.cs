fb904683d46c5d82c234a1d652256ff664d730ff
Frédéric Guay <fredericguay@hotmail.ca>
2020-09-21 12:43:26 -0400

    Version initiale - fin du cours 6

---
+++ Assets/Ennemi/EnnemiControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemiControl : MonoBehaviour, Damageable
+{
+    private GameObject player;
+    private Rigidbody myRigidBody;
+    public void TakeDamage(int damage)
+    {
+        Die();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidBody.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, 5 * Time.deltaTime));
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
